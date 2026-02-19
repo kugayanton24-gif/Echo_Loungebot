5762e7d79a1879c9196cd4042eae6c020274dcd0
kugayanton24-gif <kugayanton24@gmail.com>
2026-02-19 19:38:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,4 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     app.run_polling()
-
-if name == "main":
     main()
